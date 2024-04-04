20694a498633b7e70c56fab5175d4a17dcbaa3ab
Shivangi Mall <mallshivangi03@gmail.com>
2024-04-04 16:40:32 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+
+
+client.on("messageCreate", (message)=>{
+    if(message.author.bot)
+      return;
+    message.reply({content: "Hii from Bot"})
+});
+
+client.on("interactionCreate",(interaction)=>{
+    console.log(interaction);
+    interaction.reply("Pong!!");
+})
+
+client.login(TOKEN);
+
+
